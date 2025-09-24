74314174e9e4986f885422a803358017cc3bf331
Le Tien Thanh <letienthanhthptbs@gmail.com>
2025-09-24 23:26:03 +0700

    add shadcn

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from './Container';
+
+const Header = () => {
+    return (
+        <header>
+            <Container className='bg-red-300'> Header</Container>
+        </header>
+    );
+};
+
+export default Header;
